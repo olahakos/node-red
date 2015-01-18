7bb8a6556679c36df165b01fa1561a21a4fb929f
Wyliodrin main user <wyliodrin@vm4.wyliodrin.com>
2015-01-18 11:40:46 -0600

    nodes

--- nodes/12-wiring/ble.js
+++ nodes/12-wiring/ble.js
@@ -179,6 +179,7 @@ module.exports = function(RED) {
                 {
                     console.log ('peripheraldevice');
                     console.log (msg.peripheral);
+                    console.log (msg.peripheralUuid);
                     if (that.peripherals.get (msg.peripheralUuid) != msg.peripheral)
                     {
                         if (hasAddress (msg.peripheralUuid))
