92ab242103410085a9bfb37b48606146357e8a77
Wyliodrin main user <wyliodrin@vm4.wyliodrin.com>
2015-01-19 15:40:50 -0600

    node

--- nodes/12-wiring/ble.js
+++ nodes/12-wiring/ble.js
@@ -298,7 +298,7 @@ module.exports = function(RED) {
                                                 }
                                                 else
                                                 {
-                                                    console.log (service[0].characteristics);
+                                                    console.log (services[0].characteristics);
                                                     connected = true;
                                                     characteristics[0].notify (true, function (err)
                                                     {
